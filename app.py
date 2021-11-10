ae060e1222925c114f4e65c0456c5cfc1bf034ee
Kaustav Bhattacharjee <47206316+kaustav1900@users.noreply.github.com>
2021-11-10 19:10:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
  
 # loading the trained model
-pickle_in = open('/Users/kaustav/Documents/Python/StreamLit/cls/classifier1.pkl', 'rb') 
+pickle_in = open('classifier1.pkl', 'rb') 
 classifier = pickle.load(pickle_in)
  
 @st.cache()
